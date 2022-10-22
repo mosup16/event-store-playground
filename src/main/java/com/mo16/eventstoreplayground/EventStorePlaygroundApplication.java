3372a1e7ba52a547b9e0f5f1a708074f1a0fd4b0
mosup emad <mosup16@gmail.com>
2022-10-22 22:10:10 +0200

    add EventStore client java configurations

--- src/main/java/com/mo16/eventstoreplayground/EventStorePlaygroundApplication.java
+++ src/main/java/com/mo16/eventstoreplayground/EventStorePlaygroundApplication.java
@@ -1,13 +1,25 @@
 package com.mo16.eventstoreplayground;
 
+import com.eventstore.dbclient.EventStoreDBClient;
+import com.eventstore.dbclient.EventStoreDBConnectionString;
+import com.eventstore.dbclient.ParseError;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EventStorePlaygroundApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EventStorePlaygroundApplication.class, args);
+    }
+
+    @Bean
+    EventStoreDBClient eventStoreClient(@Value("${application.event-store.server.connection}")
+                                        String serverConn) throws ParseError {
+        var settings = EventStoreDBConnectionString.parse(serverConn);
+        return EventStoreDBClient.create(settings);
     }
 
 }
