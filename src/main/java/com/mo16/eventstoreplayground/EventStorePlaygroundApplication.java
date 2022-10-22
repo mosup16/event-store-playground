c63c1a9bcba7d630e29371160967fb159baeab29
mosup emad <mosup16@gmail.com>
2022-10-22 20:56:26 +0200

    initial commit

---
+++ src/main/java/com/mo16/eventstoreplayground/EventStorePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.mo16.eventstoreplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventStorePlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventStorePlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mo16/eventstoreplayground/EventStorePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mo16.eventstoreplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventStorePlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
